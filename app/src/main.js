84ed56055e20ce2395266b25cd1237b2d5fcb8a8
belouso4 <kibelousov379@gmail.com>
2020-04-02 23:24:33 +0300

    создал прект

--- app/src/main.js
+++ app/src/main.js
@@ -1,38 +1,7 @@
-import Vue from 'vue/dist/vue'
-import axios from 'axios'
+import Editor from "./editor";
+import "./iframe-load"
+window.editor = new Editor()
 
-
-new Vue({
-    el: "#app",
-    data: () => {
-        return {
-            "pageList": [],
-            "newPageName": ''
-        }
-
-    },
-    methods: {
-        createPage() {
-            axios
-                .post("./api/createNewHtmlPage.php", {name: this.newPageName})
-                .then(response => this.updatePage()
-                )
-        },
-        updatePage() {
-            axios
-                .get("./api/")
-                .then(response => {
-                    this.pageList = response.data
-                })
-        },
-        removePage(page) {
-            axios
-                .post("./api/removeHtmlPage.php", {remove: page})
-                .then(response => this.updatePage()
-                )
-        }
-    },
-    created() {
-        this.updatePage()
-    }
-})
+window.onload = () => {
+    window.editor.open('index.html')
+}
